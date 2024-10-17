e1c9367b6264fd0fc5048e345998336687bd5381
PMax87 <noitulove@gmail.com>
2024-10-17 14:10:39 +0200

    Login Auth

---
+++ src/firebase.tsx
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCx__foBRJLAxxwExVAdz8frIXKj8zd9_0",
+  authDomain: "react-app-25347.firebaseapp.com",
+  databaseURL:
+    "https://react-app-25347-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "react-app-25347",
+  storageBucket: "react-app-25347.appspot.com",
+  messagingSenderId: "248439625196",
+  appId: "1:248439625196:web:7340ea8b3581016e33dcc9",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
